aea1759dac9fa4fdffd067aa56254743dd193ffe
Eberson Silva <binho.ebs@gmail.com>
2026-04-01 15:07:51 -0300

    Atualização e limpeza intranet

--- intranet/assets/dist/js/nav.js
+++ intranet/assets/dist/js/nav.js
@@ -1,8 +1,21 @@
 fetch('nav.html')
   .then(res => res.text())
   .then(text => {
-    let oldelem = document.querySelector('script#replace_with_navbar')
-    let newelem = document.createElement('header')
+    const oldelem = document.querySelector('script#replace_with_navbar')
+    const newelem = document.createElement('header')
     newelem.innerHTML = text
     oldelem.parentNode.replaceChild(newelem, oldelem)
+
+    const months = [
+      'Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho',
+      'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'
+    ]
+
+    const currentMonthYear = document.getElementById('current-month-year')
+    if (currentMonthYear) {
+      const today = new Date()
+      currentMonthYear.innerHTML = '<b>' + months[today.getMonth()] + ' de ' + today.getFullYear() + '</b>'
+    }
   })
+
+  
